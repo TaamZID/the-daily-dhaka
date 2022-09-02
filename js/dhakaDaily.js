d149fde1f8b7aef9c6bceaabd196c0d644e76a62
TaamZID <tamjidalam00@gmail.com>
2022-09-03 04:29:23 +0600

    news-body-done

--- js/dhakaDaily.js
+++ js/dhakaDaily.js
@@ -10,9 +10,38 @@ const displayCategory = (categories) => {
   categories.forEach((category) => {
     const categoryDiv = document.createElement("li");
     categoryDiv.innerHTML = `
-    <li><a>${category.category_name}</a></li>
+    <li><a onclick = "newsByCategory('${category.category_id}')" >${category.category_name}</a></li>
     `;
     categoryName.appendChild(categoryDiv);
+  });
+};
+
+const newsByCategory = (id) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayCategoryNews(data.data));
+};
+
+const displayCategoryNews = (news) => {
+  const categoryNews = document.getElementById("news-body");
+  categoryNews.textContent = "";
+  news.forEach((allNews) => {
+    const newsDiv = document.createElement("div");
+    newsDiv.innerHTML = `
+        <div class="card-body">
+         <div>
+         <img src="${allNews.image_url}" alt="" width="600" height="400">
+         </div>
+          <h5 class="card-title">${allNews.title}</h5>
+          <p>${
+            allNews.details.length > 100
+              ? allNews.details.slice(0, 100) + "..."
+              : allNews.details
+          }</p>
+        </div>
+    `;
+    categoryNews.appendChild(newsDiv);
   });
 };
 
